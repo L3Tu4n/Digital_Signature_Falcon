fcf5791f4f503f082c5dd8bf1bfe25be64bdbdd9
L3Tu4n <lpatuan27@gmail.com>
2026-04-28 12:13:32 +0700

    Fix: Add missing babel plugin for private property

--- NT219-FE/src/pages/Verify/CallVerify.js
+++ NT219-FE/src/pages/Verify/CallVerify.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Result, Card, Descriptions, Spin, Alert, Tag } from "antd";
 import {
-  CheckCircleOutlined,
   WarningOutlined,
   CloseCircleOutlined,
   StopOutlined,
